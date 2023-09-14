bc2538fe6739dbebf59c2929268a2a0d2bd377cc
Joey Collins <colli384@purdue.edu>
2023-09-14 19:36:17 -0400

    Adds predictive line to puck

--- Personals/Joey/colorTrack.py
+++ Personals/Joey/colorTrack.py
@@ -2,8 +2,8 @@ import cv2
 import numpy as np
 import time
 
-video = 'airhockey.mp4'
-webcam = 0
+video = 'Personals/Joey/airhockey.mp4'
+camera = 1
 vid = cv2.VideoCapture(video)
 
 low_red = np.array([0, 150, 150])
@@ -11,11 +11,13 @@ high_red = np.array([150, 255, 200])
 old_centroid = (0, 0)
 velocity = (0, 0)
 prediction = (0, 0)
+centroid = (0, 0)
+count = 0
 
 while(True):
     ret, frame = vid.read()
-    copy = frame.copy()
     roi = frame
+
     
     if ret == True:
         
@@ -28,30 +30,44 @@ while(True):
         _, mask = cv2.threshold(mask, 254, 255, cv2.THRESH_BINARY)
         contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-        # Find largest contour, draw bounding box, find center
-        largest_contour = max(contours, key=cv2.contourArea)
-        x, y, w, h = cv2.boundingRect(largest_contour)
-        cv2.rectangle(roi, (x, y), (x+w, y+h), (0, 255, 0), 3)
-        moments = cv2.moments(largest_contour)
-        centroid = (int(moments["m10"] / moments["m00"]), int(moments["m01"] / moments["m00"]))
+        if count % 120 == 0:
+            #retrack table
+            track = 0
 
-        print(f'Velocity: {velocity}')
-        print(f'Prediction: {prediction}')
-        print(f'Actual: {centroid}\n\n')
-            
-        # Compute velocity and prediction based off of previous frame
-        velocity = (centroid[0] - old_centroid[0], centroid[1] - old_centroid[1])
-        prediction = (centroid[0] + velocity[0], centroid[1] + velocity[1]) 
-        old_centroid = centroid
+        if len(contours) > 0:
+            # Find largest contour, draw bounding box, find center
+            largest_contour = max(contours, key=cv2.contourArea)
+            x, y, w, h = cv2.boundingRect(largest_contour)
+            cv2.rectangle(roi, (x, y), (x+w, y+h), (0, 255, 0), 3)
 
+            cv2.circle(roi, (1075, 625), radius=5, color=(255,0,0), thickness=-1) #bottom right
+            cv2.circle(roi, (1075, 90), radius=5, color=(255,0,0), thickness=-1) #top right
+            cv2.circle(roi, (170, 90), radius=5, color=(255,0,0), thickness=-1) #top left
+            cv2.circle(roi, (170, 625), radius=5, color=(255,0,0), thickness=-1) #bottom left
 
+            moments = cv2.moments(largest_contour)
+            if moments["m00"] != 0:
+                centroid = (int(moments["m10"] / moments["m00"]), int(moments["m01"] / moments["m00"]))
+    
+            print(f'Velocity: {velocity}')
+            print(f'Prediction: {prediction}')
+            print(f'Actual: {centroid}\n\n')
+                
+            # Compute velocity and prediction based off of previous frame
+            velocity = (centroid[0] - old_centroid[0], centroid[1] - old_centroid[1])
+            prediction = (centroid[0] + velocity[0], centroid[1] + velocity[1]) 
+            old_centroid = centroid
+
+            cv2.line(roi, centroid, prediction, (0,255,0), 2)
 
     else:
         vid = cv2.VideoCapture('airhockey.mp4')
         continue
 
+    count += 1
+
     cv2.imshow('Frame', frame)
-    time.sleep(1)
+    time.sleep(0.4)
 
     if cv2.waitKey(1) == 27:
         break
