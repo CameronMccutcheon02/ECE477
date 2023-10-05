ab2d907952d058ab640a5b5bfb122c3f98815bdd
ahaluskaa <ahaluska@purdue.edu>
2023-10-05 16:52:45 -0400

    updated parsing test

--- Personals/Abby/stm_files/comms_test.py
+++ Personals/Abby/stm_files/comms_test.py
@@ -1,12 +1,21 @@
 import serial.tools.list_ports
 from infi.devicemanager import DeviceManager
-from serial_comms import setup_serialobject, serial_read, serial_write
+from serial_comms import setup_serialobject
+import serial 
+import serial.tools.list_ports
+import time
 
 ser = setup_serialobject()
 ser.close()
 ser.open()
 ser.flush()
-serial_write(ser, "11111111")
 
 while(True):
-    print(serial_read(ser))
+    raw_data = input("enter data: ")
+    data_to_send = str((int(raw_data, base=2) << 4))
+    while(len((data_to_send)) < 4):
+        data_to_send = "0" + data_to_send
+    data_to_send = data_to_send.encode()
+    print(data_to_send)
+    ser.write(data_to_send)
+    time.sleep(.5)
